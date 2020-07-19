ce88ae5b6880cf47ef747bf0ce31a6c9dc0021c0
Larry Shannon <larry@breakawaydesign.com>
2020-07-19 16:36:35 -0700

    Initial Commit

---
+++ Together Safely/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Together Safely
+//
+//  Created by Larry Shannon on 7/19/20.
+//  Copyright © 2020 Larry Shannon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
