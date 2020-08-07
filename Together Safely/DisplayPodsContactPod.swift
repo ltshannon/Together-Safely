dbebad57b7e287cc40f31c67b11b8bbb441e748b
Larry Shannon <lawrence.t.shannon@gmail.com>
2020-08-07 11:47:36 -0700

    Add missing files

---
+++ Together Safely/DisplayPodsContactPod.swift
@@ -0,0 +1,21 @@
+//
+//  DisplayPodsContactPod.swift
+//  Together Safely
+//
+//  Created by Larry Shannon on 8/6/20.
+//  Copyright © 2020 Larry Shannon. All rights reserved.
+//
+
+import SwiftUI
+
+struct DisplayPodsContactPod: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DisplayPodsContactPod_Previews: PreviewProvider {
+    static var previews: some View {
+        DisplayPodsContactPod()
+    }
+}
