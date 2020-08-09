36d1d22d463f48681849f1ac56374ccfc3e1e428
Jason R. Anderson <jason@kosoku.com>
2020-08-09 14:46:55 -0400

    update pods/invitations toggle menu with 50% width metrics and adjusted sizing/padding

--- Together Safely/HomeView.swift
+++ Together Safely/HomeView.swift
@@ -15,57 +15,59 @@ struct HomeView: View {
     @State private var group: Groups = Groups(id: "", name: "", members: [], riskTotals: [:], riskCompiledSring: [], riskCompiledValue: [], averageRisk: "", averageRiskValue: 0)
     
     var body: some View {
+        GeometryReader { metrics in
         VStack {
             VStack {
                 ZStack {
                     Capsule()
                         .fill((Color("Color1").opacity(49)))
-                        .frame(width: UIScreen.main.bounds.size.width - 40, height: 75)
-                    HStack {
-                         Text("Pods")
-                            .font(Font.custom("Avenir-Heavy", size: 25))
-                            .foregroundColor(self.index == 0 ? .white : .black)
-                            .fontWeight(.bold)
-                            .padding(.vertical, 10)
-                            .padding(.horizontal, 20)
-                            .background(Color("Color3").opacity(self.index == 0 ? 1 : 0))
-                            .clipShape(Capsule())
-                            .onTapGesture {
-                                 withAnimation(.default){
-                                     self.index = 0
-                                 }
-                             }
-//                            .padding(.leading, 20)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .frame(minHeight: 35, maxHeight: 35)
+                        .padding([.leading, .trailing], 10)
+                    
                         HStack {
-                             Text("Invitations")
-                                .font(Font.custom("Avenir-Heavy", size: 25))
-                                .foregroundColor(self.index == 1 ? .white : .black)
+                            Text("Pods")
+                                .font(Font.custom("Avenir-Medium", size: 18))
+                                .foregroundColor(self.index == 0 ? .white : .black)
                                 .fontWeight(.bold)
-                                .padding(.vertical, 10)
-                                .padding(.horizontal, 20)
-                            ZStack {
-                                Circle()
-                                    .fill(Color("Colorred"))
-                                    .frame(width: 45, height: 45)
-                                Text("\(firebaseService.invites.count)")
-                                    .font(.title)
-                                    .fontWeight(.heavy)
-                                    .foregroundColor(.white)
+                                .padding([.leading, .trailing], 10)
+                                .frame(width: metrics.size.width * 0.45)
+                                .background(Color("Color3").opacity(self.index == 0 ? 1 : 0))
+                                .clipShape(Capsule())
+                                .onTapGesture {
+                                    withAnimation(.default){
+                                        self.index = 0
+                                    }
+                                }
+                            Spacer()
+                            HStack {
+                                Text("Invitations")
+                                    .font(Font.custom("Avenir-Heavy", size: 18))
+                                    .foregroundColor(self.index == 1 ? .white : .black)
+                                    .fontWeight(.bold)
+                                    .padding(.leading, 10)
+                                ZStack {
+                                    Circle()
+                                        .fill(Color("Colorred"))
+                                        .frame(width: 18, height: 18)
+                                    Text("\(self.firebaseService.invites.count)")
+                                        .font(.body)
+                                        .fontWeight(.heavy)
+                                        .foregroundColor(.white)
+                                }.padding(.leading, 5).padding(.trailing, 10)
                             }
-                                .padding(.trailing, 5)
-                        }
+                            .frame(width: metrics.size.width * 0.45)
                             .background(Color("Color3").opacity(self.index == 1 ? 1 : 0))
                             .clipShape(Capsule())
                             .onTapGesture { withAnimation(.default){ self.index = 1 } }
-                    }
-                        .frame(width: UIScreen.main.bounds.size.width - 40)
+                        }
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .frame(minHeight: 30, maxHeight: 30)
+                        .padding([.leading, .trailing], 20)
+                    
                 }
-//                    .padding(.horizontal)
-//                    .padding(.top,25)
-//                    .padding(.leading, 50)
-//                    .padding(.trailing, 50)
                 Spacer()
-                if index == 0 {
+                if self.index == 0 {
                     NavigationLink(destination: CreatePodView().environmentObject(self.firebaseService)) {
                         HStack {
                             Spacer()
@@ -76,49 +78,50 @@ struct HomeView: View {
                                 .font(.title)
                                 .foregroundColor(.white).opacity(81)
                         }
-                         .padding(.trailing, 20)
+                        .padding(.trailing, 20)
                     }
                     Spacer()
-                    DisplayPodsView().environmentObject(firebaseService)
+                    DisplayPodsView().environmentObject(self.firebaseService)
                 } else {
-                    InviationsView().environmentObject(firebaseService)
+                    InviationsView().environmentObject(self.firebaseService)
                 }
             }
-            NavigationLink(destination: UserProfileView().environmentObject(firebaseService), tag: 1, selection: $action) {
+            NavigationLink(destination: UserProfileView().environmentObject(self.firebaseService), tag: 1, selection: self.$action) {
                 EmptyView()
             }
         }
-            .navigationBarTitle("")
-            .navigationBarBackButtonHidden(true)
-            .navigationBarItems(leading:
-                HStack {
-                    Button(action: {
-                        self.action = 1
-                    }) {
-                        if firebaseService.user.image != nil {
-                            Image(uiImage: UIImage(data:firebaseService.user.image!)!)
-                                .renderingMode(.original)
-                                .resizable()
-                                .frame(width: 40, height: 40)
-                                .clipShape(Circle())
-                                .overlay(Circle().stroke(Color.white, lineWidth: 1))
-                                .padding(5)
-                        } else {
-                            Image(systemName: "person.fill")
-                                .resizable()
-                                .frame(width: 40, height: 40)
-                                .clipShape(Circle())
-                                .overlay(Circle().stroke(Color.white, lineWidth: 2))
-                                .foregroundColor(Color.blue)
-                                .padding(5)
-                        }
+        .navigationBarTitle("")
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading:
+            HStack {
+                Button(action: {
+                    self.action = 1
+                }) {
+                    if self.firebaseService.user.image != nil {
+                        Image(uiImage: UIImage(data:self.firebaseService.user.image!)!)
+                            .renderingMode(.original)
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .padding(5)
+                    } else {
+                        Image(systemName: "person.fill")
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 2))
+                            .foregroundColor(Color.blue)
+                            .padding(5)
                     }
-                    Text("")
-                        .frame(width: 40, height: 10)
-                    HeaderView()
                 }
-            )
+                Text("")
+                    .frame(width: 40, height: 10)
+                HeaderView()
+            }
+        )
             .background(Image("backgroudImage").resizable().edgesIgnoringSafeArea(.all))
+        }
     }
     
     func makeuiImage(str: String) -> UIImage {
@@ -133,9 +136,9 @@ struct HomeView: View {
 }
 
 /*
-struct HomeView_Previews: PreviewProvider {
-    static var previews: some View {
-        HomeView()
-    }
-}
-*/
+ struct HomeView_Previews: PreviewProvider {
+ static var previews: some View {
+ HomeView()
+ }
+ }
+ */
